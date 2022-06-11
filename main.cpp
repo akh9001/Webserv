a1bdc7e4c10294987215af8010018ee7e1c8ffe5
lamiaa afilal <laafilal@student.1337.ma>
2022-06-12 00:56:40 +0100

    change the place of init_statusCodeMessages() from main to utility.cpp and including the utility.hpp

--- main.cpp
+++ main.cpp
@@ -6,59 +6,14 @@
 /*   By: laafilal <laafilal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 17:31:58 by akhalidy          #+#    #+#             */
-/*   Updated: 2022/06/11 12:19:14 by laafilal         ###   ########.fr       */
+/*   Updated: 2022/06/12 00:51:18 by laafilal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Includes/socket.hpp"
 #include "Parsing/Config/Config.hpp"
+#include "utility/utility.hpp"
 #include <vector>
-
-
-static std::map<std::string,std::string> statusCodeMessages;
-
-void init_statusCodeMessages()
-{
-
-	statusCodeMessages["100"] = "Continue";
-	statusCodeMessages["200"] = "OK";
-	statusCodeMessages["201"] = "Created";
-	statusCodeMessages["202"] = "Accepted";
-	statusCodeMessages["203"] = "Non-Authoritative Information";
-	statusCodeMessages["204"] = "No Content";
-	statusCodeMessages["205"] = "Reset Content";
-	statusCodeMessages["206"] = "Partial Content";
-	statusCodeMessages["300"] = "Multiple Choices";
-	statusCodeMessages["301"] = "Moved Permanently";
-	statusCodeMessages["302"] = "Found";
-	statusCodeMessages["303"] = "See Other";
-	statusCodeMessages["304"] = "Not Modified";
-	statusCodeMessages["305"] = "Use Proxy";
-	statusCodeMessages["307"] = "Temporary Redirect";
-	statusCodeMessages["400"] = "Bad Request";
-	statusCodeMessages["401"] = "Unauthorized";
-	statusCodeMessages["403"] = "Forbidden";
-	statusCodeMessages["404"] = "Not Found";
-	statusCodeMessages["405"] = "Method Not Allowed";
-	statusCodeMessages["406"] = "Not Acceptable";
-	statusCodeMessages["407"] = "Proxy Authentication Required";
-	statusCodeMessages["408"] = "Request Time-out";
-	statusCodeMessages["409"] = "Conflict";
-	statusCodeMessages["410"] = "Gone";
-	statusCodeMessages["411"] = "Length Required";
-	statusCodeMessages["412"] = "Precondition Failed";
-	statusCodeMessages["413"] = "Request Entity Too Large";
-	statusCodeMessages["414"] = "Request-URI Too Large";
-	statusCodeMessages["415"] = "Unsupported Media Type";
-	statusCodeMessages["416"] = "Requested range not satisfiable";
-	statusCodeMessages["417"] = "Expectation Failed";
-	statusCodeMessages["500"] = "Internal Server Error";
-	statusCodeMessages["501"] = "Not Implemented";
-	statusCodeMessages["502"] = "Bad Gateway";
-	statusCodeMessages["503"] = "Service Unavailable";
-	statusCodeMessages["504"] = "Gateway Time-out";
-	statusCodeMessages["505"] = "HTTP Version not supported";
-}
 
 int	main(int argc, char **argv)
 {
