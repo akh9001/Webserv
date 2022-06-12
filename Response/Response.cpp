5fb8fb63bf8fdbbc8b306c3907abce69d7ec0b22
lamiaa afilal <laafilal@student.1337.ma>
2022-06-12 04:20:51 +0100

    adding the functions getHeaders and getbody to Response

--- Response/Response.cpp
+++ Response/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: laafilal <laafilal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 12:08:59 by laafilal          #+#    #+#             */
-/*   Updated: 2022/06/11 12:21:00 by laafilal         ###   ########.fr       */
+/*   Updated: 2022/06/12 04:01:55 by laafilal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,16 @@ namespace ws {
 		
 	}
 	Response::~Response(){};
+
+	std::string Response::getHeaders(Request &request, std::string statusCode)
+	{
+		(void)request;
+		(void)statusCode;
+		return "";
+	}
+
+	std::pair<std::string, bool> Response::getbody()
+	{
+		return std::pair<std::string,bool>(this->bodyPath,this->response_is_tmp);
+	}
 }
--- Response/Response.hpp
+++ Response/Response.hpp
@@ -6,28 +6,33 @@
 /*   By: laafilal <laafilal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 12:08:56 by laafilal          #+#    #+#             */
-/*   Updated: 2022/06/11 12:15:01 by laafilal         ###   ########.fr       */
+/*   Updated: 2022/06/12 04:02:16 by laafilal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __RESPONSE_HPP__
 # define __RESPONSE_HPP__
 # include "../FileHandler/fileHandler.hpp"
+# include "../Parsing/Request/Request.hpp"
 # include <map>
 
 namespace ws {
 	
 	class Response
 	{
-		private:
-			int buildResponseTry;
 		public:
 			bool response_is_tmp;
 			
 		public:
 			Response();
 			~Response();
+			std::string getHeaders(Request &Request, std::string statusCode);
+			std::pair<std::string, bool> getbody();
 			
+		private:
+			int buildResponseTry;
+			std::string bodyPath;
+		
 	};
 
 }
