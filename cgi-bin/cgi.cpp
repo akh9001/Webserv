dc3c12b9c9f0d4e6f169d3fe4a0b833265da6836
akh9001 <akhalidy9001@gmail.com>
2022-06-23 13:04:35 +0100

    fixe some prob in cgi

--- cgi-bin/cgi.cpp
+++ cgi-bin/cgi.cpp
@@ -6,12 +6,13 @@
 /*   By: akhalidy <akhalidy@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 14:45:00 by akhalidy          #+#    #+#             */
-/*   Updated: 2022/06/23 11:38:40 by akhalidy         ###   ########.fr       */
+/*   Updated: 2022/06/23 13:03:52 by akhalidy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/cgi.hpp"
 #include <algorithm>
+#include <ostream>
 #include <string>
 
 bool	execute(const char *cmd, char **args, const char *post_body)
@@ -76,19 +77,6 @@ char	**envp(std::map<std::string, std::string> env)
 	return (envp);
 }
 
-char	**args(const char *cmd, const char * args) // cmd is the path of the file to execute & args is the file.extension(.php/ .py) to be executed
-{
-	char	**args_;
-	
-	args_ = (char **)malloc(sizeof(char *) * 3);
-	args_[0] = strdup(cmd);
-	args_[1] = strdup(args);
-	args_[2] = NULL;
-	return (args_);
-}
-
-
-
 void	set_envp(Request request, const char *script_path)
 {
 
@@ -121,41 +109,15 @@ size_t	count_occurence(const std::string &str)
 
 int	cgi(const Request &request, const char *cgi_path, const char *script_path)
 {
-	char			**args_;
-	int				i = 0;
-	std::size_t		found = 0;
-	std::size_t		len;
-	std::string		query_string;
-	std::string		arg;
-	// std::string		cgi_path;
-	
-	// cgi_path = std::string(getcwd(NULL, 0))+ "/cgi-bin/php-cgi";
-	// args_ = args(cgi_path, script_path);
-	set_envp(request, script_path);
-	if (!query_string.empty())
-	{
-		const char *args[count_occurence(query_string) + 4];
-		args[0] = cgi_path;
-		args[1] = script_path;
-		while ((len = query_string.find("&", found)) != std::string::npos)
-		{
-			args[i++] = query_string.substr(found, len).c_str();
-			found = ++len;
-		}
-		args[i++] = query_string.substr(found).c_str();
-		args[i] = NULL;
-		// for(int j = 0; j < i; j++)
-		// 	std::cout << args[j] << std::endl;
-	}
-	else
-	{
-		const char *args[3];
+	char			*args[3];
 
-		args[0] = cgi_path;
-		args[1] = script_path;
-	}
-	execute(args_[0], args_, request.getFilePath().c_str());
-	return 0;
+	set_envp(request, script_path);
+	args[0] = (char *)cgi_path;
+	args[1] = (char *)script_path;
+	args[2] = NULL;
+	if (execute(args[0], args, request.getFilePath().c_str()))
+		return true;
+	return false;
 }
 //TODO 
 //* When execve failed I should return a specific status.
