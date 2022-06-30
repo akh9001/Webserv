0b0c2c42709d39cdd6570f55f75e0268bf4e1b48
DNightOwl <laafilal@student.1337.ma>
2022-06-30 18:55:19 +0100

    clean comments

--- Response/Response.cpp
+++ Response/Response.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mokhames <mokhames@student.42.fr>          +#+  +:+       +#+        */
+/*   By: laafilal <laafilal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 12:08:59 by laafilal          #+#    #+#             */
-/*   Updated: 2022/06/30 18:46:37 by mokhames         ###   ########.fr       */
+/*   Updated: 2022/06/30 18:55:06 by laafilal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,7 +124,6 @@ namespace ws {
 			else
 			{
 				this->statusCode = "302";
-				//TODO
 				originErrorPath = ltrim(originErrorPath);
 				originErrorPath = buildLocationPath(originErrorPath, request);
 				setHeader("Location",originErrorPath);
@@ -172,22 +171,11 @@ namespace ws {
 
 	std::string Response::buildPath(std::string &resourcePath)
 	{
-		// std::string slash;
-		// std::string s ;
 		std::string root;
-		// if(this->currentLocation.getLocation_match()  == "/")
-		// 	slash = "/";
 
 		root = this->currentLocation.getRoot() + "/";
-		// if(!resourcePath.empty() && resourcePath.at(0) != '/')
-		// std::cout << "res path " << resourcePath << std::endl;
-		// std::cout << "loc " << this->currentLocation.getLocation_match() << std::endl;
-		// std::cout << "root " << root << std::endl;
-
 		std::string path = resourcePath.replace(0,this->currentLocation.getLocation_match().length(),root);
 
-		// std::string path =    root + slash + resourcePath;
-		// std::cout<<"result path " << path << std::endl;
 		return path;
 	}
 
@@ -217,17 +205,8 @@ namespace ws {
 				redirectionPath = buildLocationPath(redirectionPath, request);
 				setHeader("Location",redirectionPath);
 				buildResponse(request);
+				throw "Redirection";
 			}
-			else
-			{
-				//TODO to be tested at school// not to be handeled 
-				// std::string tmpDirectory = ("./response_tmp_files");
-				// std::string tmpPath = ws::fileHandler::createTmp(tmpDirectory);
-				// ws::fileHandler::write(tmpPath,redirectionPath);
-				// this->bodyPath = tmpPath;
-				// this->response_is_tmp = true;
-			}
-			throw "Redirection";
 		}
 	}
 	
@@ -420,9 +399,6 @@ namespace ws {
 		}
 		else
 		{
-			// std::string keepRoot = "";
-			// std::string root = buildPath(keepRoot);
-			// std::string keepRoot = "";
 			std::string root = this->currentLocation.getRoot();
 			if(!ws::fileHandler::checkIfExist(root))
 			{
@@ -536,7 +512,6 @@ namespace ws {
 			}
 
 			absoluteResourcePath = this->currentLocation.getUploadPath() + request.getUri().replace(0,this->currentLocation.getLocation_match().length(),"");
-			// absoluteResourcePath = buildPath(uploadPath);
 			
 			if(ws::fileHandler::checkIfExist(absoluteResourcePath))
 			{
@@ -547,15 +522,12 @@ namespace ws {
 			else
 			{
 				std::string tmpFile = request.getFilePath();
-				// std::vector<std::string> dirList = pathSpliter(uploadPath);
-
-				// int ret = directoriesHandler(dirList[0], dirList, 0,absoluteResourcePath);
-				////////////////////
+				
 				int ret = 1;
 				size_t pos = absoluteResourcePath.find_last_of('/');
 				std::string dirs = absoluteResourcePath.substr(0,pos);
 				int pathStat = stat(dirs.c_str(), &this->fileStat);
-				// std::string pathCmd ;	
+
 				if( pathStat != 0)
 				{	
 					int flag = 0;
@@ -592,7 +564,6 @@ namespace ws {
 						ret =  500;
 				}
 
-				///////////////////
 				if(ret == 1)
 				{
 					int err = rename(tmpFile.c_str(),absoluteResourcePath.c_str());
@@ -604,9 +575,6 @@ namespace ws {
 					}
 					else
 					{
-						// uploadPath = ltrim(uploadPath);
-						// uploadPath = buildLocationPath(uploadPath, request);
-						// setHeader("Location",uploadPath); no need
 						this->statusCode = "201";
 						this->bodyPath.clear();
 						setHeader("Content-Length","0");
@@ -661,68 +629,9 @@ namespace ws {
 		return dirList;
 	}
 
-	// int Response::directoriesHandler(std::string filename, std::vector<std::string> dirList, int i, std::string originPath)
-	// {
-	// 	std::cerr << filename << std::endl;
-	// 	int ret = 0;
-	// 	std::string path = buildPath(filename);
-	// 	if(i < (int)dirList.size() - 2)
-	// 	{	
-	// 		i++;
-	// 		std::string newf = filename+"/"+dirList[i];
-	// 		ret = directoriesHandler(newf,dirList,i,originPath);
-	// 	}
-	// 	if(ret != 0)
-	// 		return ret;
-
-	// 	int pathStat = stat(path.c_str(), &this->fileStat);
-	// 	// std::string pathCmd ;	
-	// 	if( pathStat != 0)
-	// 	{	
-	// 		size_t pos = originPath.find_last_of('/');
-	// 		std::string dirs = ltrim(originPath.substr(0,pos));
-	// 		int flag = 0;
-
-	// 		for (size_t i = 0; i < dirs.length(); i++)
-	// 		{
-	// 			if(dirs[i] == '/')
-	// 			{
-	// 				dirs[i] = '\0';
-	// 				if(mkdir(dirs.c_str(),0777) != 0)
-	// 					flag = 1;
-	// 				else
-	// 					flag = 0;
-	// 				dirs[i] = '/';
-	// 			}
-	// 		}
-	// 		if(mkdir(dirs.c_str(),0777) != 0)
-	// 			flag = 1;
-	// 		else
-	// 			flag = 0;
-	// 		if(flag)
-	// 			return 500;
-	// 		return 1;
-	// 	}
-	// 	if(pathStat == 0)
-	// 	{
-	// 		if( this->fileStat.st_mode & S_IFDIR )
-	// 		{	
-	// 			if(this->fileStat.st_mode & S_IWUSR)
-	// 				return 1;
-	// 			else if (!(this->fileStat.st_mode & S_IWUSR))
-	// 				return 403;
-	// 		}
-	// 		else
-	// 			return 409;
-	// 	}
-
-	// 	return ret;
-	// }
-
 	void Response::checkCgi(std::string &resourcePath, Request &request)
 	{
 		std::string filePath = resourcePath;
-		// std::cout <<"file path ================ 1 "<< filePath << std::endl;
 		if(ws::fileHandler::checkIfExist(resourcePath))
 		{
 			if(isDir(resourcePath))
@@ -747,7 +656,6 @@ namespace ws {
 				}
 			}
 		}
-		// std::cout <<"file path ================ 2"<< filePath << std::endl;
 		request.cgi_ptr = new CGI();
 		request.cgi_ptr->cgi(request, getCgiPath().c_str(), filePath.c_str());
 		throw "calling cgi";
@@ -977,7 +885,6 @@ namespace ws {
 	void Response::craftDeleteResponse(Request &request) 
 	{
 		std::string absoluteResourcePath = buildAbsolutePath(request);
-		// std::cout << absoluteResourcePath << std::endl;
 		try
 		{
 			isResourceValid(absoluteResourcePath,request);
@@ -1098,14 +1005,3 @@ namespace ws {
   		return r;
 	}
 }
-
-
-//TODO paths as subject !!!
-
-//TODO response_tmp_files and request_tmp_files
-//TODO /dir/../../test
-//flaging all tmp as true to be deleted
-//TODO test DELETE AND POST AND GET
-// add / to dir in auto index
-//change log msgs to more clear ones
-//check put nginx in case no fie name
